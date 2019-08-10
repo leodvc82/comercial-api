09cb9ba86b577b40cdcb3670da020e1682743469
Leonardo <Leonardo@192.168.25.3>
2019-08-10 18:25:49 -0300

    correcao

--- src/main/java/com/comercial/exception/ResourceNotFoundException.java
+++ src/main/java/com/comercial/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-	private static final long serialVersionUID = 457331855650901453L;
+	private static final long serialVersionUID = 457331855650901453L; 
 
 	public ResourceNotFoundException(String message) {
         super(message);
